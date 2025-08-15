b049d66d0f7f04a6686a59f5fdb34a2e7484ff60
Bhavik <bhavikpahwa2007@gmail.com>
2025-08-16 02:41:10 +0530

    Setup Commit

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='w-full bg-oxford_blue'>
+      <h1 className=' text-orange_web'>Blogspot</h1>
+    </div>
+  )
+}
+
+export default Hero
